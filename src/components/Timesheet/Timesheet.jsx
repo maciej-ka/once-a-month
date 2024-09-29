4dea90e5f122f866c0a2c6225f51adcc91452a6c
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-09-29 15:54:06 +0200

    feat: basic timeshet header data

--- src/components/Timesheet/Timesheet.jsx
+++ src/components/Timesheet/Timesheet.jsx
@@ -1,13 +1,18 @@
+import PojoDate from 'pojodate';
+
+const timesheetDate = new PojoDate('2024-09-01')
 const data = {
   name: "Frontend development projects",
-  period: new Date('2024-09-01'),
+  period: timesheetDate.toLocaleString('default', { month: 'long' }) + ' ' + timesheetDate.years,
+  date: timesheetDate.add({ months: 1 }).set({ days: 0 })
 }
 
 const Timesheet = () => {
   return (
     <div>
       <div>{data.name}</div>
-      <div>{data.period.toLocaleString('default', { month: 'long' })}</div>
+      <div>{data.period.toUpperCase()}</div>
+      <div>{data.date.format(({ years, months, days }) => `${days}/${months}/${years}`)}</div>
     </div>
   )
 }
