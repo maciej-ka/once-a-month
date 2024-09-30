49d746c171eb9df19976f90dd0b9a5ff04b1491a
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-09-30 11:39:01 +0200

    feat: component for a timesheet day

---
+++ src/components/Timesheet/Day.tsx
@@ -0,0 +1,19 @@
+import PojoDate from 'pojodate';
+
+type Props = {
+  date: PojoDate
+}
+
+const Day = ({ date }: Props) => {
+  return (
+    <>
+      <div className="bg-blueberry border border-black">{date.format(({years, months, days}) => `${days}/${months}/${years}`)}</div>
+      <div className="border border-black text-black font-normal"></div>
+      <div className="border border-black text-black font-normal"></div>
+      <div className="border border-black text-black font-normal"></div>
+      <div className="border border-black text-black font-normal"></div>
+    </>
+  )
+}
+
+export default Day
--- src/components/Timesheet/Timesheet.tsx
+++ src/components/Timesheet/Timesheet.tsx
@@ -1,4 +1,6 @@
 import PojoDate from 'pojodate';
+
+import Day from './Day';
 import logo from "../../logo.png"
 
 type Props = {
@@ -10,12 +12,19 @@ const Timesheet = ({ date }: Props) => {
   const monthEnd = monthStart.add({ months: 1 }).set({ days: 0 });
   const monthName = date.toLocaleString('default', { month: 'long' }).toUpperCase() + ' ' + monthEnd.years
 
+  const days: PojoDate[] = [];
+  let current = monthStart;
+  while (current <= monthEnd) {
+    days.push(current);
+    current = current.add({ days: 1 })
+  }
+
   const localStorageData = {
     username: "Frontend development projects",
   }
 
   return (
-    <div className="grid grid-cols-5 text-center font-bold text-white border border-black">
+    <div className="grid grid-cols-5 text-center font-bold text-white border border-black leading-snug">
       <div className="bg-raspberry">NAME:</div>
       <div className="bg-raspberry">{localStorageData.username}</div>
       <div className="col-span-3 row-span-7"><img alt="logo" src={logo}/></div>
@@ -23,17 +32,18 @@ const Timesheet = ({ date }: Props) => {
       <div className="bg-raspberry">{monthName}</div>
       <div className="bg-raspberry">DATE:</div>
       <div className="bg-raspberry">{monthEnd.format(({ years, months, days }) => `${days}/${months}/${years}`)}</div>
-      <div className="col-span-2 h-6" />
+      <div className="col-span-2 h-6"/>
       <div className="bg-raspberry">Number of HOURS TOTAL:</div>
       <div className="bg-raspberry text-left">[Hours]</div>
       <div className="bg-raspberry">Number of DAYS TOTAL:</div>
       <div className="bg-raspberry text-left">[MD]</div>
-      <div className="col-span-2 h-6" />
+      <div className="col-span-2 h-6"/>
       <div className="bg-blueberry border border-black">Date</div>
       <div className="bg-blueberry border border-black">Hours</div>
       <div className="bg-blueberry border border-black">Tasks</div>
       <div className="bg-blueberry border border-black">Task ID</div>
       <div className="bg-blueberry border border-black">Comments</div>
+      {days.map(day => <Day date={day} />)}
     </div>
   )
 }
